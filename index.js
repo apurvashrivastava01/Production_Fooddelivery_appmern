ac924501835736d7c77f7cce45219a500b1bb165
apurvashrivastava01 <215919apurvashrivastavamscit@gmail.com>
2023-04-24 18:52:24 +0530

    final code
    food delivery mern stack

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv").config();
+const bodyParser = require('body-parser');
+require("./config/db")
+const cors = require('cors');
+const path =require('path');
+
+// mongoose.connect('mongodb://localhost:27017/mern-ecommerce');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+app.use(express.static(path.join(__dirname,'./client/build')));
+
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+
+app.use("/customers", require("./routes/customerRoutes"))
+app.use("/categories", require("./routes/categoryRoutes"))
+app.use("/products", require("./routes/productRoutes"))
+app.use("/carts", require("./routes/cartRoutes"))
+app.use("/orders", require("./routes/orderRoutes"))
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
+
