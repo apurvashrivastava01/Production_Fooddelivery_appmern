7a3ad6ee29f90991f4b674deda8d50a17d3dc9d2
apurvashrivastava01 <131666531+apurvashrivastava01@users.noreply.github.com>
2023-04-25 22:13:43 +0530

    UPDATE

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose')
-const DB =process.env.MONGO_URL;
+const DB =mongodb+srv://sona:01071998@cluster0.fyn8tfk.mongodb.net/mern_fooddelivery;
 mongoose.connect(DB,{
     useNewUrlParser:true
 }).then(()=>console.log("connection start")).catch((error)=>console.log(error.message))
